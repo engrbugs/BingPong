115517ca233cb69e259e2b70898e2807a4725528
engrbugs <engrbugs@gmail.com>
2023-05-22 21:59:21 -0700

    2023-05-22-2158

--- BingPong.py
+++ BingPong.py
@@ -5,12 +5,12 @@ from wonderwords import RandomWord
 
 def generate():
     r = RandomWord()
-    for i in range(30):
+    for i in range(40):
         word = r.word(include_parts_of_speech=["nouns"])
         print(word)
         os.system(f"start microsoft-edge:https://www.bing.com/news/search?q={word}"
                   f"+news&form=hdrsc6")
-        time.sleep(5)
+        time.sleep(3)
 
 
 if __name__ == '__main__':
